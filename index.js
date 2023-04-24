c001f00812b6fc926ee2384ba946f83dd3dd34b3
bobur2006coder <boburdilmurodov702@gmail.com>
2023-04-24 17:52:50 +0500

    fix

---
+++ index.js
@@ -0,0 +1,103 @@
+let a = +prompt("oylarni kiriting")
+switch (a) {
+    case 1:
+        console.log("qizil");
+        break;
+        case 2:
+            console.log("sabzi rang");
+            break;
+            case 3:
+                console.log("juda yomon");
+                break;
+        
+    
+
+
+    default:
+        break;
+}
+
+// switch (a) {
+//     case 2:
+//         console.log("qoniqarsiz");
+//         break;
+//         case 3:
+//             console.log("qoniqarli");
+//             break;
+//             case 4:
+//                 console.log("yaxshi");
+//                 break;
+//                 case 5:
+//                     console.log("a'lo");
+//                     break;
+            
+        
+    
+
+//     default:
+//         console.log("bunday son mavjud emas");
+//         break;
+// }
+
+// switch (a) {
+//     case 7:
+//         console.log("ronaldo");
+        
+//         break;
+//         case 9:
+//         console.log("benzema");
+        
+//         break;
+//         case 10:
+//         console.log("messi");
+        
+//         break;
+
+//     default:
+//         console.log("xato");
+//         break;
+// }
+
+
+// switch (a) {
+//     case 1:
+//         console.log("Yanvar");
+//         break;
+//         case 2:
+//         console.log("fevral");
+//         break;
+//         case 3:
+//         console.log("mart");
+//         break;
+//         case 4:
+//         console.log("Aprel");
+//         break;
+//         case 5:
+//         console.log("May");
+//         break;
+//         case 6:
+//         console.log("Iyun");
+//         break;
+//         case 7:
+//         console.log("iyul");
+//         break;
+//         case 8:
+//         console.log("Avgust");
+//         break;
+//         case 9:
+//         console.log("sentabr");
+//         break;
+//         case 10:
+//         console.log("oktabr");
+//         break;
+//         case 11:
+//         console.log("noyabr");
+//         break;
+//         case 12:
+//         console.log("dekabr");
+//         break;
+
+//     default:
+//         console.log("xato ma`lumot");
+//         break;
+// }
